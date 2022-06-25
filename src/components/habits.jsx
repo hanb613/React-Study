c1b0eb0989f68e8bcabd54f6bf6b6bba02675541
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-06-25 13:09:16 +0900

    Feat: 기본 구조

---
+++ src/components/habits.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Habit from "./habit";
+
+class Habits extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    /*
+    habit.count++;
+    this.setState(this.state);
+    */ // 이렇게 해도 되지만, 리액트에서는 state를 직접 수정하는 것이 좋지 않다.
+
+    const habitsArr = [...this.state.habits];
+    const index = habitsArr.indexOf(habit);
+
+    habitsArr[index].count++;
+    this.setState({ habits: habitsArr });
+  };
+
+  handleDecrement = (habit) => {
+    const habitsArr = [...this.state.habits];
+    const index = habitsArr.indexOf(habit);
+    const count = habitsArr[index].count - 1;
+
+    habitsArr[index].count = count < 0 ? 0 : count;
+    this.setState({ habits: habitsArr });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.state.habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Habits;
