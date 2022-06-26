68b944b819be08e358a7b0c9cd87171a136d6e3d
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-06-26 13:58:15 +0900

    fix: addForm & PureComponent

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./app.css";
 import Habits from "./components/habits";
 import Navbar from "./components/navbar";
-import AddForm from "./components/addForm";
 
 class App extends Component {
   state = {
@@ -14,18 +13,27 @@ class App extends Component {
   };
 
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-    this.setState({ habit });
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1 };
+      }
+      return item;
+    });
+
+    this.setState({ habits });
   };
 
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habit });
+    const habits = this.state.habits.map((item) => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+
+      return item;
+    });
+
+    this.setState({ habits });
   };
 
   handleDelete = (habit) => {
@@ -40,9 +48,12 @@ class App extends Component {
 
   handleReset = () => {
     const habits = this.state.habits.map((habit) => {
-      habit.count = 0;
+      if (habit.count !== 0) {
+        return { ...habit, count: 0 };
+      }
       return habit;
     });
+
     this.setState({ habits });
   };
 
@@ -52,12 +63,12 @@ class App extends Component {
         <Navbar
           totalCount={this.state.habits.filter((item) => item.count > 0).length}
         />
-        <AddForm onAdd={this.handleAdd} />
         <Habits
           habits={this.state.habits}
           onIncrement={this.handleIncrement}
           onDecrement={this.handleDecrement}
           onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
           onReset={this.handleReset}
         />
       </>
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Habit from "./habit";
+import AddForm from "./addForm";
 
 class Habits extends Component {
   render() {
     return (
       <>
         <div className="habits">
+          <AddForm onAdd={this.props.onAdd} />
           <ul>
             {this.props.habits.map((habit) => (
               <Habit
