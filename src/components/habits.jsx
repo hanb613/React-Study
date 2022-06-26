dc33cd81e98ef7f761d035468268286fbc5158bd
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-06-26 16:01:07 +0900

    fix: React Hooks

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,31 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import Habit from "./habit";
 import AddForm from "./addForm";
 
-class Habits extends Component {
-  render() {
-    return (
-      <>
-        <div className="habits">
-          <AddForm onAdd={this.props.onAdd} />
-          <ul>
-            {this.props.habits.map((habit) => (
-              <Habit
-                key={habit.id}
-                habit={habit}
-                onIncrement={this.props.onIncrement}
-                onDecrement={this.props.onDecrement}
-                onDelete={this.props.onDelete}
-              />
-            ))}
-          </ul>
-          <button className="habits-reset" onClick={this.props.onReset}>
-            Reset All
-          </button>
-        </div>
-      </>
-    );
-  }
-}
+const Habits = ({
+  habits,
+  onIncrement,
+  onDecrement,
+  onDelete,
+  onAdd,
+  onReset,
+}) => {
+  return (
+    <div className="habits">
+      <AddForm onAdd={onAdd} />
+      <ul>
+        {habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            onDelete={onDelete}
+          />
+        ))}
+      </ul>
+      <button className="habits-reset" onClick={onReset}>
+        Reset All
+      </button>
+    </div>
+  );
+};
 
 export default Habits;
