5ddbaabc4a67c2d196246af3fd95a5307a03e053
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-07-03 14:08:32 +0900

    feat : show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./app.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  return <h1>Hello </h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAnchU5C1ra1ZfEQoBjBq-3QpAolXxUH4Q",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
