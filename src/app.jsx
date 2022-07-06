9b77381e2068a3fad6c896d48c75f19e765a1e7f
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-07-06 16:13:39 +0900

    feat : 검색 기능

--- src/app.jsx
+++ src/app.jsx
@@ -5,27 +5,24 @@ import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import VideoSearch from "./components/video_search/video_search";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAnchU5C1ra1ZfEQoBjBq-3QpAolXxUH4Q",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
     <div className={styles.app}>
-      <VideoSearch />
+      <VideoSearch onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
--- src/components/video_search/video_search.jsx
+++ src/components/video_search/video_search.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./video_search.module.css";
 
-const VideoSearch = (props) => (
-  <header className={styles.header}>
-    <div className={styles.logo}>
-      <img
-        className={styles.img}
-        src={process.env.PUBLIC_URL + "/images/logo.png"}
-        alt="Youtube logo"
+const VideoSearch = ({ onSearch }) => {
+  const inputRef = useRef();
+
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    onSearch(value);
+  };
+
+  const onClick = () => {
+    handleSearch();
+  };
+
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img
+          className={styles.img}
+          src={process.env.PUBLIC_URL + "/images/logo.png"}
+          alt="Youtube logo"
+        />
+        <h2 className={styles.title}>Youtube</h2>
+      </div>
+      <input
+        className={styles.input}
+        type="search"
+        placeholder="검색"
+        onKeyDown={onKeyDown}
+        ref={inputRef}
       />
-      <h2 className={styles.title} c>
-        Youtube
-      </h2>
-    </div>
-    <input className={styles.input} type="search" placeholder="검색" />
-    <button className={styles.button} type="submit">
-      <img
-        className={styles.buttonImg}
-        src={process.env.PUBLIC_URL + "/images/search.png"}
-      ></img>
-    </button>
-  </header>
-);
+      <button className={styles.button} type="submit" onClick={onClick}>
+        <img
+          className={styles.buttonImg}
+          src={process.env.PUBLIC_URL + "/images/search.png"}
+        ></img>
+      </button>
+    </header>
+  );
+};
 
 export default VideoSearch;
