06b307574c2407348b134ad7b741af88419af6c8
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-07-27 16:47:20 +0900

    add : 싫어요 카운트 추가 및 수정

--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import styles from "./video_detail.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FaRegThumbsUp, FaRegThumbsDown, FaThumbsUp } from "react-icons/fa";
+import {
+  FaRegThumbsUp,
+  FaRegThumbsDown,
+  FaThumbsUp,
+  FaThumbsDown,
+} from "react-icons/fa";
 
 const VideoDetail = ({
   video,
@@ -10,16 +14,26 @@ const VideoDetail = ({
   check,
 }) => {
   const [number, setNumber] = useState(Number(statistics.likeCount));
-  const [thumbsCheck, setThumbsCheck] = useState(check[0]);
+  const [LikeCheck, setLikeCheck] = useState(check[0]);
+  const [DisLikeCheck, setDisLikeCheck] = useState(!check[0]);
 
   const OnThumbsUp = () => {
-    if (thumbsCheck === false) {
+    if (LikeCheck === false) {
       setNumber((number) => number + 1);
-      setThumbsCheck(!thumbsCheck);
     } else {
       setNumber((number) => number - 1);
-      setThumbsCheck(!thumbsCheck);
     }
+    setLikeCheck(!LikeCheck);
+    setDisLikeCheck(true);
+  };
+
+  const OnThumbsDown = () => {
+    if (LikeCheck === true) {
+      setNumber((number) => number - 1);
+    }
+
+    setDisLikeCheck(!DisLikeCheck);
+    setLikeCheck(false);
   };
 
   return (
@@ -35,12 +49,14 @@ const VideoDetail = ({
           frameBorder="0"
           allowFullScreen
         ></iframe>
+
         <h2>{snippet.title} </h2>
+
         <div className={styles.video_info}>
           조회수 {Number(statistics.viewCount).toLocaleString("en-US")}회
           <div className={styles.thumbs}>
             <button className={styles.thumbsUp} onClick={OnThumbsUp}>
-              {thumbsCheck ? (
+              {LikeCheck ? (
                 <FaThumbsUp className={styles.thumbsUpIcon} />
               ) : (
                 <FaRegThumbsUp className={styles.thumbsUpIcon} />
@@ -52,14 +68,14 @@ const VideoDetail = ({
                   : "좋아요"}
               </span>
             </button>
-            <button className={styles.thumbsDown}>
-              <FaRegThumbsDown />
-              <span>
-                {Number(statistics.dislikeCount).toLocaleString("en-US") !==
-                "NaN"
-                  ? Number(statistics.dislikeCount).toLocaleString("en-US")
-                  : "싫어요"}
-              </span>
+
+            <button className={styles.thumbsDown} onClick={OnThumbsDown}>
+              {DisLikeCheck ? (
+                <FaRegThumbsDown className={styles.thumbsDownIcon} />
+              ) : (
+                <FaThumbsDown className={styles.thumbsDownIcon} />
+              )}
+              <span>싫어요</span>
             </button>
           </div>
         </div>
