bdcf96d21f82f7a41fdd30851b80271a5bdd08a6
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-08-03 15:50:54 +0900

    add : 이미지 업로드 시 로딩 스피너

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,8 +1,9 @@
 import { async } from "@firebase/util";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
 
   const onButtonClick = (event) => {
@@ -11,9 +12,10 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
   };
 
   const onChange = async (event) => {
-    console.log(event.target.files[0]);
+    setLoading(true);
     //imageUploader.upload(event.target.files[0]).then(console.log);
     const uploaded = await imageUploader.upload(event.target.files[0]);
+    setLoading(false);
 
     onFileChange({
       name: uploaded.original_filename,
@@ -31,9 +33,16 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         name="file"
         onChange={onChange}
       />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || "No file"}
-      </button>
+
+      {!loading && (
+        <button
+          className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+          onClick={onButtonClick}
+        >
+          {name || "No file"}
+        </button>
+      )}
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
