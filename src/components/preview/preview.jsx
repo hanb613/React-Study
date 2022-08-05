90acd692c293ac512f2f8c9e1217fcaebdeab457
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-08-05 10:58:32 +0900

    fix : preview 수정

--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -5,7 +5,7 @@ import styles from "./preview.module.css";
 const Preview = ({ cards }) => (
   <section className={styles.preview}>
     <h1 className={styles.title}>Card Preview</h1>
-    <ul className={styles}>
+    <ul className={styles.cards}>
       {Object.keys(cards).map((key) => (
         <Card key={key} card={cards[key]} />
       ))}
